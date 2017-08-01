d3bc75f3ddc1d840c7535b7afb00a7a1b1b698d6
octopus-prime <mike.gresens@gmail.com>
2017-08-01 19:48:19 +0200

    More tests

--- test/uri.cpp
+++ test/uri.cpp
@@ -15,7 +15,6 @@ BOOST_AUTO_TEST_SUITE(test)
 
 //"https://horst.com:123?foo%20xxx#bar",
 //"https://he.wikipedia.org/wiki/%D7%9E%D7%9C%D7%97%D7%9E%D7%94_%D7%95%D7%A9%D7%9C%D7%95%D7%9D",
-//"http://www.w%33.org",
 //"http://r%C3%A4ksm%C3%B6rg%C3%A5s.josefsson.org",
 //"http://%E7%B4%8D%E8%B1%86.w3.mag.keio.ac.jp",
 
@@ -23,29 +22,34 @@ std::initializer_list<std::string> const strings
 {
 	"http://www.ics.uci.edu:65000/pub/ietf/uri/?foo=bar#Related",
 	"https://foo:bar@www.google.com",
+	"http://www.w%33.org",
 };
 
 std::initializer_list<std::string> const schemes
 {
 	"http",
 	"https",
+	"http",
 };
 
 std::initializer_list<std::string> const userinfos
 {
 	"",
 	"foo:bar",
+	"",
 };
 
 std::initializer_list<std::string> const hosts
 {
 	"www.ics.uci.edu",
 	"www.google.com",
+	"www.w3.org",
 };
 
 std::initializer_list<std::uint16_t> const ports
 {
 	65000,
+	0,
 	0,
 };
 
@@ -53,17 +57,20 @@ std::initializer_list<std::string> const paths
 {
 	"/pub/ietf/uri/",
 	"",
+	"",
 };
 
 std::initializer_list<std::string> const queries
 {
 	"foo=bar",
 	"",
+	"",
 };
 
 std::initializer_list<std::string> const fragments
 {
 	"Related",
+	"",
 	"",
 };
 
